f9a708242970dfc00263a045dff48a48a364b2b3
Hadi Mottale <HadiMottale@gmail.com>
2026-06-20 20:13:57 +0330

    chore(release): bump version to 0.1.3
    - feat: expand color palette
    - fix: resolve documentation build warnings and grid layout syntax
    - refactor: improve documentation structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import re
 project = 'Asha'
 copyright = '2026-, آزاداندیش داده‌ساز'
 author = 'Dataist'
-release = '0.1.2'
+release = '0.1.3'
 
 # -- General Configuration ---------------------------------------------------
 extensions = [
